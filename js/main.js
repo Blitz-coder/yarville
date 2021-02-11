836aa3b3c84945abe23ed0525f24ff2350601581
Blitz-coder <blitz-coder@narod.ru>
2021-02-11 23:53:56 +0700

    create: add swiper slider

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 $(document).ready(function () {
 
-
+    const swiper = new Swiper('.swiper-container', {
+    
+        loop: true,
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      
+        
+      });
 
 });
